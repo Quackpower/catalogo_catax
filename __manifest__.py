d6643f6f30e07891a1f2dfc5ce2d76eb5553f33e
Quackpower <Quackpower70@gmail.com>
2022-08-17 18:32:30 -0500

    x

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "catalogos_catax",
+    'name': "catalogos_reporte",
 
     'summary': """
         Short (1 phrase/line) summary of the module's purpose, used as
